c4768989adbb89812252ded46c4f9c105eed7405
Josh <jsmith07@uw.edu>
2018-09-20 11:55:47 -0700

    copy michaels data models from github.

--- pypi/data/__all_models.py
+++ pypi/data/__all_models.py
@@ -1,4 +1,18 @@
-# noinspection PyUnresolvedReferences
-from pypi.data.packages import Package
-
 # import all classes we define.
+
+
+# noinspection PyUnresolvedReferences
+import pypi.data.downloads
+# noinspection PyUnresolvedReferences
+import pypi.data.languages
+# noinspection PyUnresolvedReferences
+import pypi.data.licenses
+# noinspection PyUnresolvedReferences
+import pypi.data.maintainers
+# noinspection PyUnresolvedReferences
+import pypi.data.packages
+# noinspection PyUnresolvedReferences
+import pypi.data.releases
+# noinspection PyUnresolvedReferences
+import pypi.data.users
+
