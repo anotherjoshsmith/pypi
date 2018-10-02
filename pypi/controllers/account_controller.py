b965b101f868a5854151105aa02ad4493766bc6a
Josh <jsmith07@uw.edu>
2018-10-02 09:34:21 -0700

    setup registeration and login.

--- pypi/controllers/account_controller.py
+++ pypi/controllers/account_controller.py
@@ -1,7 +1,11 @@
+from pyramid.request import Request
 from pyramid.view import view_config
+import pyramid.httpexceptions as x
 
 
 # #################### INDEX ############################
+from pypi.services import user_services
+
 
 @view_config(route_name='account_home',
              renderer='pypi:templates/account/index.pt',
@@ -15,15 +19,36 @@ def index(_):
 @view_config(route_name='login',
              renderer='pypi:templates/account/login.pt',
              request_method='GET')
-def login_get(request):
-    return {}
+def login_get(_):
+    return {
+        'email': None,
+        'password': None,
+        'error': None
+    }
 
 
 @view_config(route_name='login',
              renderer='pypi:templates/account/login.pt',
              request_method='POST')
 def login_post(request):
-    return {}
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+
+    # create new user
+    user = user_services.login_user(email, password)
+
+    if not user:
+        return {
+            'email': email,
+            'password': password,
+            'error': 'The user could not be found, '
+                     'or the password is incorrect.'
+        }
+
+    # TODO: Login user (session)
+
+    return x.HTTPFound('/account')
+
 
 
 # #################### REGISTRATION ######################
@@ -32,14 +57,35 @@ def login_post(request):
              renderer='pypi:templates/account/register.pt',
              request_method='GET')
 def register_get(_):
-    return {}
+    return {
+        'email': None,
+        'name': None,
+        'password': None,
+        'error': None
+    }
 
 
 @view_config(route_name='register',
              renderer='pypi:templates/account/register.pt',
              request_method='POST')
-def register_post(request):
-    return {}
+def register_post(request: Request):
+    email = request.POST.get('email')
+    name = request.POST.get('name')
+    password = request.POST.get('password')
+
+    if not email or not name or not password:
+        return {
+            'email': email,
+            'name': name,
+            'password': password,
+            'error': 'Some required fields are missing.'
+        }
+
+    # create new user
+    user = user_services.create_user(email, name, password)
+    # TODO: Login user
+
+    return x.HTTPFound('/account')
 
 
 # #################### LOGOUT ############################
